fb8561d3589d88bfcc8109d6ac970a051b24a09a
DevOpsMaster76 <practicedevops56@gmail.com>
2024-01-03 18:19:37 +0530

    terrform variables

---
+++ variables/variables.tf
@@ -0,0 +1,41 @@
+variable "ami_id" {
+  type = string
+  default = "ami-03265a0778a880afb"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
+
+variable "tags" {
+  type = map
+  default = {
+    Name = "Hello Terraform"
+    component = "web"
+    Project ="Roboshop"
+    Environment = "DEV"
+    Terraform = "true"
+  }
+
+}
+
+variable "sg-name" {
+  type = string
+  default = "roboshop-all"
+}
+
+variable "sg-description" {
+  type = string
+  default = "Allowing all ports"
+}
+
+variable "inbound-from-port" {
+  type = number
+  default = 0
+}
+
+variable "cidr_blocks" {
+  type = list
+  default = ["0.0.0.0/0"]
+}
